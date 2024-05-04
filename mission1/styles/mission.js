e1bf0be8ec3b9f77c0e980cfbd7f29105729627d
Isaac <icemaun@gmail.com>
2024-05-04 17:36:34 -0600

    mission 2

---
+++ mission1/styles/mission.js
@@ -0,0 +1,20 @@
+const themeSelector = document.getElementById('dark')
+
+function changeTheme() {
+
+const selectedOption = themeSelector.value;
+
+if (selectedOption === 'dark') {
+    
+    document.body.classList.add('dark');
+    
+    document.getElementById('byui-logo').src = 'images/byui-logo_white.webp';
+} else {
+    
+    document.body.classList.remove('dark');
+    
+    document.getElementById('byui-logo').src = 'images/byui-logo_blue.webp';
+}
+}
+
+themeSelector.addEventListener('click', changeTheme);
