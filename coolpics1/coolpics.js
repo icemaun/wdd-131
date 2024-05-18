4b0fb0f9c96c96119bd05d6259641012371c20ae
Isaac <icemaun@gmail.com>
2024-05-18 15:33:06 -0600

    6

--- coolpics1/coolpics.js
+++ coolpics1/coolpics.js
@@ -27,14 +27,20 @@ function handleResize() {
 
   function viewHandler(event) {
 	// create a variable to hold the element that was clicked on from event.target
+    const clickedElement = event.target;
 
 	// get the src attribute from that element and 'split' it on the "-"
+    const srcParts = clickedElement.src.split("-");
 
 	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    const newSrc = srcParts[0] + "-full.jpeg";
 
 	// insert the viewerTemplate into the top of the body element
 	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate);
 
 	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    const closeButton = document.getElementById("closeButton");
+	closeButton.addEventListener("click", closeViewer);
 
 }
